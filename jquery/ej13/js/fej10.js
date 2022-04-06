99c3cec5e11979afc88fc22876e0c2dfc290fd8e
tasagaya <angelbmw@gmail.com>
2022-04-06 11:35:46 +0100

    del jueves 4

---
+++ jquery/ej13/js/fej10.js
@@ -0,0 +1,31 @@
+var x;
+x=$(document);
+x.ready(inicio);
+
+function inicio(){
+	var x=$("#boton1");
+	x.click(extraerTexto);
+
+	x=$("#boton2");
+	x.click(modificarTexto);
+
+	x=$("#boton3");
+	x.click(modificarDatosTabla);
+}
+
+function extraerTexto()
+{
+	var x=$("#parrafo1");
+	alert(x.text());
+}
+
+function modificarTexto()
+{
+	var x=$("#parrafo1");
+	x.text("Nuevo texto en el párrafo");
+}
+function modificarDatosTabla()
+{
+	var x=$("td");
+	x.text("texto nuevo celda");
+}
